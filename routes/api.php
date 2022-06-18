e5f195deccc4f3f2c60506ec837c0c5bf2b210f0
marian-roller <webmaster@rollercoastery.pl>
2022-06-18 15:39:43 +0200

    basic auth working routes with controllers and jwt tokens flow

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Auth'], function() {
+    Route::post('signin', 'SignInController');
+    Route::post('signout', 'SignOutController');
+    Route::get('me', 'MeController');
 });
