498fb01372695c19032e2304d860ea89b1fcf726
marian-roller <webmaster@rollercoastery.pl>
2022-06-22 21:55:38 +0200

    enabled registering user from postman and from frontend, no validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,18 +4,34 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+
     public function __invoke(Request $request) {
 
-        $object = $request->all();
+        // $this->validate($request, [
+        //     'name' => ['required', 'string', 'max:255'],
+        //     'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        //     'password' => ['required', 'string', 'min:8', 'confirmed'],
+        // ]);
 
-        return response()->json([
-            'my_data' => $object
+        
+
+        $data = $request->all();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
         ]);
 
-        // dd($object);
+        return response()->json([
+            'user' => $data
+        ]);
 
     }
 }
