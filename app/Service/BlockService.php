fec7d232c2794ea210bd4a6c08c7e4bcd42d4009
marian-roller <webmaster@rollercoastery.pl>
2022-06-28 19:57:47 +0200

    minor refactor on block service

--- app/Service/BlockService.php
+++ app/Service/BlockService.php
@@ -6,21 +6,14 @@ class BlockService implements BlockServiceInterface {
 
     public function mine(array $data): array {
 
-    
-        $algo = $data['algorithm'];
-        $blockId = $data['blockId'];
         $nonce = $data['nonce'];
-        $input = $data['data'];
-        
         $hash = $data['hash'];
-        $condition = $data['hashStart'];
-
-        // return [$condition];
-
-
-        while (substr($hash, 0, strlen($condition)) !== $condition) {
+       
+        while (substr($hash, 0, strlen($data['hashStart'])) !== $data['hashStart']) {
             $nonce += 1;
-            $hash = $this->convert($blockId . $nonce . $input, $algo);
+            $hash = $this->convert(
+                $data['blockId'] . $nonce . $data['data'], 
+                $data['algorithm']);
         }
 
         return ['nonce' => $nonce, 'hash' => $hash];
