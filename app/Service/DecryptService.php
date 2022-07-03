298b5ce8e3664153345ad000a129ecf7237cd126
marian-roller <webmaster@rollercoastery.pl>
2022-07-03 11:28:05 +0200

    adjust encryption, decryption first working flow

--- app/Service/DecryptService.php
+++ app/Service/DecryptService.php
@@ -5,10 +5,24 @@ namespace App\Service;
 class DecryptService implements DecryptServiceInterface {
 
     public function decrypt(array $data) {
-        
-        $result = $data;
 
-        return $result;
+        $result = 'unsuccesful decryption';
+        $cipher = 'aes-' . $data['keysize'] . '-' . $data['mode'];
+        
+        $decoded_encrypted_data = base64_decode($data['message']);
+
+        $ivlen = openssl_cipher_iv_length($cipher);
+        $iv = substr($decoded_encrypted_data, 0, $ivlen);
+        $decoded_encrypted_message = substr($decoded_encrypted_data, $ivlen);
+
+        $decoded_decrypted_message = openssl_decrypt(
+            $decoded_encrypted_message, 
+            $cipher, 
+            $data['key'], 
+            $options=OPENSSL_RAW_DATA, 
+            $iv);
+
+        return $decoded_decrypted_message;
     }
 
     
