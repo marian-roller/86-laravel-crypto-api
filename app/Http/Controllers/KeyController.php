4e5e9b0392143f2c333e4627e33d3fed520aec1f
marian-roller <webmaster@rollercoastery.pl>
2023-02-05 18:43:55 +0100

    params to openssl keys generator

--- app/Http/Controllers/KeyController.php
+++ app/Http/Controllers/KeyController.php
@@ -9,7 +9,11 @@ class KeyController extends Controller {
 
 
     public function generateKeys() {
-        $pair = openssl_pkey_new();
+        $pair = openssl_pkey_new([
+            "digest_alg"=>'sha256',
+            "private_key_bits" => 2048,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        ]);
         openssl_pkey_export($pair, $privKey);
 
         $public_key = openssl_pkey_get_details($pair)['key'];
